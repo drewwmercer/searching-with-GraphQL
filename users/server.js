34c215c1a2cc8daede62e2f70fc1f7dd2c9c240e
Drew Mercer <japanexpress@gmail.com>
2018-01-20 16:27:48 -0500

    Registering GraphQL with Express

--- users/server.js
+++ users/server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const expressGraphQL = require('express-graphql');
 
 const app = express();
+
+app.use('/graphql', expressGraphQL({
+    graphiql: true
+}));
 
 app.listen(4000, () => {
   console.log('Express app is listening.');
