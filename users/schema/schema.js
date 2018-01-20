6c517a0bc6b8f590edaacb2534d4da854235e910
Drew Mercer <japanexpress@gmail.com>
2018-01-20 16:59:36 -0500

    Finishing schema setup

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -1,10 +1,17 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 
 const {
     GraphQLObjectType,
     GraphQLString,
-    GraphQLInt
+    GraphQLInt,
+    GraphQLSchema
 } = graphql;
+
+const users = [
+    { id: '23', firstName: 'Bill', age: 20 },
+    { id: '47', firstName: 'Samantha', age: 21 }
+];
 
 const UserType = new GraphQLObjectType({
     name: 'User',
@@ -13,4 +20,21 @@ const UserType = new GraphQLObjectType({
         firstName: { type: GraphQLString },
         age: { type: GraphQLInt }
     }
+});
+
+const rootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    user: {
+        type: UserType,
+        args: { id: { type: GraphQLString } },
+        resolve(parentValue, args) {
+            return _.find(users, { id: args.id });
+        }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+    query: rootQuery
 });
--- users/server.js
+++ users/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const expressGraphQL = require('express-graphql');
+const schema = require('./schema/schema');
 
 const app = express();
 
 app.use('/graphql', expressGraphQL({
-    graphiql: true
+  schema,  
+  graphiql: true,
 }));
 
 app.listen(4000, () => {
