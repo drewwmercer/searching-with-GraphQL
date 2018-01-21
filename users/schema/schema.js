b204aa6e7abf556ac403f42f8de6853a3072fe28
Drew Mercer <japanexpress@gmail.com>
2018-01-21 11:10:40 -0500

    Adding Company type object

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -1,37 +1,42 @@
 const graphql = require('graphql');
 const axios = require('axios');
 
-const {
-    GraphQLObjectType,
-    GraphQLString,
-    GraphQLInt,
-    GraphQLSchema
-} = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
+
+const CompanyType = new GraphQLObjectType({
+  name: 'Company',
+  fields: {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString }
+  }
+});
 
 const UserType = new GraphQLObjectType({
-    name: 'User',
-    fields: {
-        id: { type: GraphQLString },
-        firstName: { type: GraphQLString },
-        age: { type: GraphQLInt }
-    }
+  name: 'User',
+  fields: {
+    id: { type: GraphQLString },
+    firstName: { type: GraphQLString },
+    age: { type: GraphQLInt }
+  }
 });
 
 const rootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
     user: {
-        type: UserType,
-        args: { id: { type: GraphQLString } },
-        resolve(parentValue, args) {
-            // return _.find(users, { id: args.id });
-            return axios.get(`http://localhost:3000/users/${args.id}`)
-            .then(resp => resp.data);
-        }
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        // return _.find(users, { id: args.id });
+        return axios
+          .get(`http://localhost:3000/users/${args.id}`)
+          .then(resp => resp.data);
+      }
     }
   }
 });
 
 module.exports = new GraphQLSchema({
-    query: rootQuery
+  query: rootQuery
 });
