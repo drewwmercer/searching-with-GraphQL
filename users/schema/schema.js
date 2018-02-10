5aa7d81dfc3f4356c0d8495b21149c3434446a73
Drew Mercer <japanexpress@gmail.com>
2018-02-10 10:44:58 -0500

    Adding concurrently and delete mutation

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -85,7 +85,14 @@ const mutation = new GraphQLObjectType({
       }
     },
     deleteUser: {
-      
+      type: UserType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString) }
+      },
+      resolve(parentValue, { id }) {
+        return axios.delete(`http://localhost:3000/users/${id}`)
+        .then(res => res.data);
+      }
     }
   }
 });
