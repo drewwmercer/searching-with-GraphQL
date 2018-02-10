ba0e50e714d971a316ca7a70264e67be44dcccd8
Drew Mercer <japanexpress@gmail.com>
2018-02-10 10:54:59 -0500

    Adding edit user mutation

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -80,8 +80,9 @@ const mutation = new GraphQLObjectType({
         companyId: { type: GraphQLString }
       },
       resolve(parentValue, { firstName, age }) {
-        return axios.post(`http://localhost:3000/users`, { firstName, age })
-        .then(res => res.data);
+        return axios
+          .post(`http://localhost:3000/users`, { firstName, age })
+          .then(res => res.data);
       }
     },
     deleteUser: {
@@ -90,8 +91,23 @@ const mutation = new GraphQLObjectType({
         id: { type: new GraphQLNonNull(GraphQLString) }
       },
       resolve(parentValue, { id }) {
-        return axios.delete(`http://localhost:3000/users/${id}`)
-        .then(res => res.data);
+        return axios
+          .delete(`http://localhost:3000/users/${id}`)
+          .then(res => res.data);
+      }
+    },
+    editUser: {
+      type: UserType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString) },
+        firstName: { type: GraphQLString },
+        age: { type: GraphQLInt },
+        companyId: { type: GraphQLString }
+      },
+      resolve(parentValue, args) {
+        return axios
+          .patch(`http://localhost:3000/users/${args.id}`, args)
+          .then(res => res.data);
       }
     }
   }
